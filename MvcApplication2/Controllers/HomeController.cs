27cfb7daa70fad86194ff90e9203aafadea1c44d
darrenji <764190362@qq.com>
2014-06-30 23:59:27 +0800

    moved

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication2.Models;
+
+namespace MvcApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["c"] = post.Content;
+                return PartialView("Result");
+            }
+            return View(post);
+        }
+
+    }
+}
---
+++ MvcApplication2/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication2.Models
+{
+    public class Post
+    {
+        [Required(ErrorMessage = "必填")]
+        [DataType(DataType.MultilineText)]
+        public string Content { get; set; }
+    }
+}
